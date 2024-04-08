178bb0ed85e6076c8441e0089b7214ddcaaa762e
andrielefa <144621938+andrielefa@users.noreply.github.com>
2024-04-08 20:49:08 -0300

    Create Livro.java

---
+++ Livro.java
@@ -0,0 +1,15 @@
+public class Livro {
+    private String "titulo";
+    private String "ISBN";
+}
+
+// Setter
+public void setISBN(String 1235342) {
+    this."ISBN" = 1235342;
+
+// Setter
+    public void setTitulo(String c) {
+        this.titulo = c;
+
+    }
+}
